ed6840395ac397ce5646945e9d429fcd17eb58e8
xSyki <mikolajsykula@gmail.com>
2022-09-18 12:28:04 +0200

    Update package.json

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-    "transform": {
-        "^.+\\.js$": "babel-jest",
-        ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
-        ".+\\.(css|scss|png|jpg|svg)$": "jest-transform-stub"
-      },
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+    '.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
+  },
 }
